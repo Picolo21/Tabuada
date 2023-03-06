9eb2c84f0c3b968650e401492772232483416ef1
Vinicius Picolo <picolo.vininicius@gmail.com>
2023-03-05 23:22:44 -0300

    Add project files.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Informe um valor para calcular a tabuada: ");
+        int value = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine($"{value} x {i} = {value * i}");
+        }
+    }
+}
